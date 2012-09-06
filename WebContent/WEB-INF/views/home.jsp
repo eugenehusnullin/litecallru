95720a22804fffcc424ca11f01e458af7acff4a5
goalinternet <goalinternet@gmail.com>
2012-09-06 23:58:29 +0300

    Вывод списка очередей

--- WebContent/WEB-INF/views/home.jsp
+++ WebContent/WEB-INF/views/home.jsp
@@ -1,6 +1,6 @@
-<?xml version="1.0" encoding="ISO-8859-1" ?>
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
  
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -9,6 +9,15 @@
 <title>Home</title>
 </head>
 <body>
-	<h1>Hello world!</h1>
+	<div> <c:out value="${username}"/> </div>
+	
+	<form action="request">
+		<select id="queue">
+			<c:forEach items="${queues}" var="qi">
+				<option value="${qi}">${qi}</option>
+			</c:forEach>
+		</select>
+	</form>
+	
 </body>
 </html>
--- src/ru/maks105fm/web/HomeController.java
+++ src/ru/maks105fm/web/HomeController.java
@@ -1,6 +1,8 @@
 package ru.maks105fm.web;
 
 import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,20 +11,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ru.maks105fm.dao.Dao;
+
 @Controller
 public class HomeController {
+
+	@Autowired
+	Dao dao;
 
 	@Autowired
 	Comparator<String> comparator;
 
 	@RequestMapping(value = "/")
-	public String home() {
-		System.out.println("HomeController: Passing through...");
+	public String home(Model model) {
+		String username = "user1";
+		model.addAttribute("username", username);
+
+		List<String> queues = dao.getQueues(username);
+		model.addAttribute("queues", queues);
+
+		return "home";
+	}
+
+	@RequestMapping(value = "/request", method = RequestMethod.GET)
+	public String request(@RequestParam("queue") String queue,
+			@RequestParam("from") Date from, @RequestParam("to") Date to) {
+		
 		return "home";
 	}
 
 	@RequestMapping(value = "/compare", method = RequestMethod.GET)
-	public String compare(@RequestParam("input1") String input1, @RequestParam("input2") String input2, Model model) {
+	public String compare(@RequestParam("input1") String input1,
+			@RequestParam("input2") String input2, Model model) {
 		int result = comparator.compare(input1, input2);
 		String inEnglish = (result < 0) ? "less than"
 				: (result > 0 ? "greater than" : "equal to");
